4fdd80bd03fd050b4f692e5a182f57b32f465b0a
Fei Deng <fdeng@neurosky.com>
2015-08-21 16:02:26 -0700

    finish q025, add q029

--- src/com/fei/projecteuler/Part2.java
+++ src/com/fei/projecteuler/Part2.java
@@ -1,6 +1,7 @@
 package com.fei.projecteuler;
 
 import java.math.BigInteger;
+import java.util.HashSet;
 
 public class Part2 {
 
@@ -12,12 +13,32 @@ public class Part2 {
 	
 	// 1000-digit Fibonacci number
 	public static void q025() {
-		for(int i = 1; ; i++) {
-			BigInteger t = BigInteger.valueOf(fibonacci(i));
-			if (t.toString().length() >= 10) {
+//		for(int i = 1; ; i++) {
+//			BigInteger t = BigInteger.valueOf(fibonacci(i));
+//			if (t.toString().length() >= 10) {
+//				System.out.println(i);
+//				break;
+//			}
+//		}
+		
+		BigInteger lowerthres = BigInteger.TEN.pow(10 - 1);
+		BigInteger upperthres = BigInteger.TEN.pow(10);
+		BigInteger prev = BigInteger.ONE;
+		BigInteger cur = BigInteger.ZERO;
+		int i = 0;
+		while (true) {
+			// At this point, prev = fibonacci(i - 1) and cur = fibonacci(i)
+			if (cur.compareTo(lowerthres) >= 0) {
 				System.out.println(i);
 				break;
 			}
+			else if (cur.compareTo(upperthres) >= 0)
+				throw new RuntimeException("Not found");
+			
+			BigInteger temp = cur.add(prev);
+			prev = cur;
+			cur = temp;
+			i++;
 		}
 	}
 	
@@ -27,9 +48,27 @@ public class Part2 {
 		}
 		return fibonacci(n - 1) + fibonacci(n - 2);
 	}
+	
+	//Distinct powers
+	public static void q029() {
+		HashSet<BigInteger> dis = new HashSet<BigInteger>();
+		int count = 0;
+		for (int i = 2; i <= 100; i++) {
+			for (int j = 2; j <= 100; j++) {
+				BigInteger tem = BigInteger.valueOf(i).pow(j);
+				if (!dis.contains(tem)) {
+					dis.add(tem);
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		q025();
+		q029();
 	}
 
 }
